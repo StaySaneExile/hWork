9d15548b059595761119b5cc0dba9b47e8022989
StaySaneExile <vasiliy2490@gmail.com>
2020-04-22 17:32:02 +0500

    Updates

---
+++ precache-manifest.25a0470bea091d032360ab24f9fc5de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5216fdad3ee3dd1084ad9fa2ef6438cf",
+    "url": "/hWork/index.html"
+  },
+  {
+    "revision": "5906f358eb3e14e47d00",
+    "url": "/hWork/static/css/main.619b3dc6.chunk.css"
+  },
+  {
+    "revision": "cc372b3dc5a29a833411",
+    "url": "/hWork/static/js/2.7a5080ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hWork/static/js/2.7a5080ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5906f358eb3e14e47d00",
+    "url": "/hWork/static/js/main.3602e9d4.chunk.js"
+  },
+  {
+    "revision": "0b73e350ed9517d97ca9",
+    "url": "/hWork/static/js/runtime-main.5f6b128c.js"
+  }
+]);
